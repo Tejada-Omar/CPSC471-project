62dbd3647cc4d67471ef1a96bd38aa455dc2fc60
Omar Tejada <72728094+Tejada-Omar@users.noreply.github.com>
2025-04-16 03:50:41 -0600

    Mirror typescript eslint rules for unused vars

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -6,6 +6,22 @@ export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
   {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+  {
     languageOptions: {
       parserOptions: {
         projectService: true,
