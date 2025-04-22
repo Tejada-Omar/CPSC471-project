64b783a47313a3b7203c5cfaff0b8a975527684c
Omar Tejada <72728094+Tejada-Omar@users.noreply.github.com>
2025-04-22 00:36:08 -0600

    feat: cancel unapproved loan requests from user dashboard

--- client/src/Components/UserPage/UserPage.jsx
+++ client/src/Components/UserPage/UserPage.jsx
@@ -1,38 +1,20 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {
-  Box,
-  Button,
-  Stack,
-  Typography,
-  Card,
-  CardContent,
-  Paper,
-} from "@mui/material";
+import { Box, Button, Typography, Paper } from "@mui/material";
 import { API_URL } from "../../utils/constants";
 
 import "./UserStyles.css";
+import LoansList from "./Components/LoansList";
 
 const UserPage = () => {
   const authToken = localStorage.getItem("authToken");
   const navigate = useNavigate();
 
-  const [loanData, setLoanData] = useState([]);
-  const [requestedLoanData, setRequestedLoanData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [userData, setUserData] = useState({});
 
   const fetchData = async () => {
     try {
-      const response = await fetch(`${API_URL}/loan/user`, {
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-        },
-      });
-      const data = await response.json();
-
-      setLoanData(data);
-
       const userResponse = await fetch(`${API_URL}/user/ownUser`, {
         headers: {
           Authorization: `Bearer ${authToken}`,
@@ -42,18 +24,6 @@ const UserPage = () => {
       const userDataTemp = await userResponse.json();
 
       setUserData(userDataTemp);
-
-      const requestedLoanResponse = await fetch(
-        `${API_URL}/loan/user?approved={false}`,
-        {
-          headers: {
-            Authorization: `Bearer ${authToken}`,
-          },
-        }
-      );
-      const requestLoanData = await requestedLoanResponse.json();
-
-      setRequestedLoanData(requestLoanData);
     } catch (err) {
       console.error("Failed to fetch data:", err);
     } finally {
@@ -108,50 +78,6 @@ const UserPage = () => {
     navigate(url);
   };
 
-  // Check if a loan is overdue
-  const isOverdue = (retDate) => {
-    const today = new Date();
-    return new Date(retDate) < today;
-  };
-
-  // Function to handle the return book API call
-  const returnBook = async (loan) => {
-    try {
-      const userResponse = confirm(
-        `Do you want to return book ${loan.book.title}?`
-      );
-      console.log(loan);
-
-      if (userResponse) {
-        console.log("Returning book with Loan ID:", loan.loan.id);
-
-        // API call
-        const response = await fetch(`${API_URL}/loan/${loan.loan.id}`, {
-          method: "DELETE",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${authToken}`,
-          },
-          body: JSON.stringify({
-            bookId: loan.book.id,
-            authorId: loan.book.authorId,
-          }),
-        });
-
-        if (!response.ok) {
-          throw new Error("Failed to return the book");
-        }
-
-        fetchData();
-
-        // Successfully returned book, update UI or show success
-        alert(`Book ${loan.book.title} has been returned successfully!`);
-      }
-    } catch (error) {
-      alert("Error returning the book: " + error.message);
-    }
-  };
-
   if (isLoading) {
     return <>Loading...</>;
   }
@@ -167,117 +93,8 @@ const UserPage = () => {
         elevation={3}
         sx={{ padding: 5, borderRadius: 2, boxShadow: 3, mt: 3 }}
       >
-        <Typography variant="h5" sx={{ textDecoration: "underline" }}>
-          My Current Loans
-        </Typography>
-
-        {/* Generate a card for each loan */}
-        {loanData.map((loan, index) => {
-          const overdue = isOverdue(loan.loan.retDate); // Check if the loan is overdue
-
-          return (
-            <Card
-              key={loan.loan.id}
-              sx={{
-                mt: 2,
-                p: 2,
-                backgroundColor: overdue ? "#ffe6e6" : "white", // Slightly red tint for overdue loans
-                border: overdue ? "1px solid red" : "1px solid grey",
-                boxShadow: 2,
-                borderRadius: 2,
-              }}
-            >
-              <CardContent>
-                <Stack spacing={2}>
-                  <Typography variant="h6">Loan ID: {loan.loan.id}</Typography>
-                  <Typography variant="h6">
-                    Book Title: {loan.book.title}
-                  </Typography>
-                  <Typography variant="body1">
-                    <strong>Synopsis:</strong> {loan.book.synopsis}
-                  </Typography>
-                  <Typography variant="body1">
-                    <strong>Loan Start Date:</strong>{" "}
-                    {new Date(loan.loan.startDate).toLocaleDateString()}
-                  </Typography>
-                  <Typography variant="body1">
-                    <strong>Return Date:</strong>{" "}
-                    {new Date(loan.loan.retDate).toLocaleDateString()}
-                  </Typography>
-                  {overdue && (
-                    <Typography
-                      variant="body2"
-                      color="red"
-                      sx={{ fontStyle: "italic" }}
-                    >
-                      This loan is overdue!
-                    </Typography>
-                  )}
-
-                  {/* "Return Book" Button */}
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={() => returnBook(loan)}
-                  >
-                    Return Book
-                  </Button>
-                </Stack>
-              </CardContent>
-            </Card>
-          );
-        })}
-        <Typography variant="h5" mt={6} sx={{ textDecoration: "underline" }}>
-          My Requested Loans
-        </Typography>
-
-        {/* Generate a card for each loan */}
-        {requestedLoanData.map((loan, index) => {
-          const overdue = isOverdue(loan.loan.retDate); // Check if the loan is overdue
-
-          return (
-            <Card
-              key={loan.loan.id}
-              sx={{
-                mt: 2,
-                p: 2,
-                backgroundColor: overdue ? "#ffe6e6" : "white", // Slightly red tint for overdue loans
-                border: overdue ? "1px solid red" : "1px solid grey",
-                boxShadow: 2,
-                borderRadius: 2,
-              }}
-            >
-              <CardContent>
-                <Stack spacing={2}>
-                  <Typography variant="h6">Loan ID: {loan.loan.id}</Typography>
-                  <Typography variant="h6">
-                    Book Title: {loan.book.title}
-                  </Typography>
-                  <Typography variant="body1">
-                    <strong>Synopsis:</strong> {loan.book.synopsis}
-                  </Typography>
-                  <Typography variant="body1">
-                    <strong>Loan Start Date:</strong>{" "}
-                    {new Date(loan.loan.startDate).toLocaleDateString()}
-                  </Typography>
-                  <Typography variant="body1">
-                    <strong>Return Date:</strong>{" "}
-                    {new Date(loan.loan.retDate).toLocaleDateString()}
-                  </Typography>
-                  {overdue && (
-                    <Typography
-                      variant="body2"
-                      color="red"
-                      sx={{ fontStyle: "italic" }}
-                    >
-                      This loan is overdue!
-                    </Typography>
-                  )}
-                </Stack>
-              </CardContent>
-            </Card>
-          );
-        })}
+        <LoansList title={"My Current Loans"} isApproved={true} />
+        <LoansList title={"My Requested Loans"} isApproved={false} />
       </Paper>
     </Box>
   );
