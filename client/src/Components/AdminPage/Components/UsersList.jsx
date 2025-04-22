d7bd0f910883b633ce7be29b29944917802fdc8b
StvnNgyn1 <89041795+StvnNgyn1@users.noreply.github.com>
2025-04-22 05:42:43 -0600

    fix: sign up phone number fixed feat: users display on admin dashboard

---
+++ client/src/Components/AdminPage/Components/UsersList.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import { Stack, Typography, Card, CardContent, Box } from "@mui/material";
+import { API_URL } from "../../../utils/constants";
+
+import RemoveUserBtton from "./removeUserButton"
+
+const UsersList = ({ title }) => {
+  const authToken = localStorage.getItem("authToken");
+
+  const [userData, setUserData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const loanResponse = await fetch(`${API_URL}/user/allUsers`, {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+
+      const userData = await loanResponse.json();
+      setUserData(userData);
+    } catch (err) {
+      console.error("Failed to fetch data:", err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <>Loading...</>;
+  }
+
+  return (
+    <>
+      <Box mt={5}>
+        <Typography variant="h5" sx={{ textDecoration: "underline" }}>
+          {title}
+        </Typography>
+
+        {/* Generate a card for each loan */}
+        {userData.map((user, index) => {
+
+          return (
+            <Card
+              key={user.user_id}
+              sx={{
+                mt: 2,
+                p: 2,
+                boxShadow: 2,
+                borderRadius: 2,
+              }}
+            >
+              <CardContent>
+                <Stack spacing={2}>
+                <Typography variant="h6">User ID: {user.user_id}</Typography>
+                  <Typography variant="h6">Name: {user.uname}</Typography>
+                  <Typography variant="h6">Address: {user.address}</Typography>
+                  <Typography variant="h6">Phone Number: {user.phone_no}</Typography>
+                </Stack>
+              </CardContent>
+            </Card>
+          );
+        })}
+      </Box>
+    </>
+  );
+};
+
+export default UsersList;
